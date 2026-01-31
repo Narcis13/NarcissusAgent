263b185c0143d5ee444482294ccabbf760397aa2
Narcis13 <Narcis75@gmail.com>
2026-01-31 16:28:14 +0200

    feat(01-02): create PTY module index exports
    - Exports PTYManager class and all types from module
    - Enables clean imports: import { PTYManager } from "./pty"

---
+++ src/pty/index.ts
@@ -0,0 +1,9 @@
+/**
+ * PTY Module
+ *
+ * Exports PTY management functionality for spawning and controlling
+ * Claude Code in a pseudo-terminal.
+ */
+
+export { PTYManager } from "./manager";
+export * from "./types";
